642e4f9a8860c15994f2a1528926bcd0809cc2ed
Andrew Roskuski <aroskuski@gmail.com>
2014-12-14 14:14:25 -0500

    Comments in routes

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,18 @@ router.get('/wishlist.html', function(req, res){
   res.render('wishlist', { title: 'Wishlist' });
 });
 
+//takes no arguments, returns the json to put in the chart
 router.get('/pricedata.json', wishlist.pricedata);
 
+//takes no arguments, returns html fragment
 router.get('/topitems.html', wishlist.topitems);
 
+//name, link, and price must be in body, returns wishlist item id
+// returns HTTP 400 if error, but this shouldn't happen
 router.post('/addlistitem', wishlist.additem);
 
+//id, name, link, and price must be in body
+//returns HTTP 204 on success, HTTP 400 in error
 router.post('/modifylistitem', wishlist.modifyitem);
 
 module.exports = router;

05004bc988f957d6c6977d7ae7ab4498236d2f76
argallo <argallo@wpi.edu>
2014-12-15 00:59:08 -0500

    added example charts

---
+++ public/js/charts.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Gallo on 12/15/2014.
+ */
+
+
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
